d1cc84a462652497e6d3b0f00da2ec9ff1730163
kelvybaby <kelvinzhou2003@gmail.com>
2025-02-24 18:53:31 -0500

    gs proc

--- python 02:2025/test.py
+++ python 02:2025/test.py
@@ -14,6 +14,7 @@ show()
 '''
 
 'kelvin'
+'''
 def guess_the_number() -> None:
     secret_number = 7  # The number to guess
     guess = int(input("Guess a number between 1 and 10: "))  # Get user input
@@ -25,5 +26,42 @@ def guess_the_number() -> None:
 
 
 guess_the_number()
+'''
+
+def frobenius_inner_product(A, B):
+    """Computes the Frobenius inner product of two matrices A and B."""
+    return np.trace(B.T @ A)  # Equivalent to sum of elementwise products
 
 
+def gram_schmidt_frobenius(matrices):
+    """Applies the Gram-Schmidt process using the Frobenius inner product."""
+    orthogonal_basis = []
+
+    for A in matrices:
+        U = np.copy(A)  # Copy the matrix to modify it
+
+        for prev_U in orthogonal_basis:
+            # Compute projection coefficient
+            proj_coeff = frobenius_inner_product(A, prev_U) / frobenius_inner_product(prev_U, prev_U)
+            # Subtract projection
+            U -= proj_coeff * prev_U
+
+        orthogonal_basis.append(U)
+
+    return np.array(orthogonal_basis)
+
+
+# Given basis matrices
+v1 = np.array([[1, 1], [1, -2]])
+v2 = np.array([[-2, -1], [0, 2]])
+v3 = np.array([[-6, -2], [-2, 9]])
+
+basis_matrices = [v1, v2, v3]
+
+# Compute the orthogonal basis
+orthogonal_basis = gram_schmidt_frobenius(basis_matrices)
+
+# Print the orthogonal basis
+print("Orthogonal Basis Matrices:")
+for i, mat in enumerate(orthogonal_basis, 1):
+    print(f"U_{i}:\n", mat, "\n")
